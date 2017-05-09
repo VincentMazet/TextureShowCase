d1e492426365e68efb2d6b7a906353103668680b
Vincent Mazet <vincentmazet42@gmail.com>
2017-05-09 21:33:56 +0200

    added ui button

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+	public Button lightsButton;
+	public GameObject[] lights;
+
+	void Start () {
+		Button lightsBtn = lightsButton.GetComponent<Button>();
+		lightsBtn.onClick.AddListener(actionLights);
+
+		lights = GameObject.FindGameObjectsWithTag("RoomLight");
+	}
+	
+	void actionLights () {
+
+		foreach(GameObject light in lights){
+			float intensity = light.GetComponent<Light> ().intensity;
+
+			if (intensity == 0) {
+				light.GetComponent<Light> ().intensity = 2.64f;
+			} else {
+				light.GetComponent<Light> ().intensity = 0;
+			}
+		}
+	}
+}
